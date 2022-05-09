fec7205f629daed3cccf7bbfff78aeb1c44fdb48
deveper <semsnt12@gmail.com>
2022-05-09 20:57:03 +0200

    AlertMessage and Todos Added LocalStorage -dynamic Alert Element

--- TodoApp/wwwroot/js/todo.js
+++ TodoApp/wwwroot/js/todo.js
@@ -1,23 +1,73 @@
 ﻿
+//Elementleri seçtik
 const form = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo");
 const todoList = document.querySelector(".list-group");
 const firstCardBody = document.querySelectorAll(".card-body")[0];
 const secondCardBody = document.querySelectorAll(".card-body")[1];
+const addButton = document.querySelector("#btn btn-danger");
 const filter = document.querySelector("#filter");
 const clearButton = document.querySelector("#clear-todos");
 
 eventListeners();
 function eventListeners() {
-    form.addEventListener("submit", addTodo)
+    form.addEventListener("submit", addTodo);
 }
 
 function addTodo(e) {
     const newTodo = todoInput.value.trim();
-    addTodoToUI(newTodo);
+    if (newTodo != "") {
+        showAlert("success", "Bir Todo Oluşturuldu");
+        addTodoToUI(newTodo);
+        addTodoToLocalStorage(newTodo);
+    }
+    else {
+        showAlert("danger", "Todo Girmediniz");
+    }
     e.preventDefault();
 
 }
+
+function getTodosFromStorage() {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    return todos;
+
+
+
+}
+function addTodoToLocalStorage(newTodo) {
+
+    let todos = getTodosFromStorage();
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+}
+
+function showAlert(type, message) {
+    //     <div class="alert alert-danger" role="alert">
+    //   A simple danger alert—check it out!
+    // </div>
+    const alertDiv = document.createElement("div");
+    alertDiv.className = `alert alert-${type}`;
+    alertDiv.textContent = message;
+    if (type === "danger") {
+        todoInput.style.visibility = 'hidden';
+    }
+    firstCardBody.appendChild(alertDiv);
+    setTimeout(function () {
+        alertDiv.remove();
+        todoInput.style.visibility = 'visible';
+    }, 1000);
+}
+
+
+
 function addTodoToUI(newTodo) {
 
     //  <li class="list-group-item d-flex justify-content-between">
@@ -33,7 +83,7 @@ function addTodoToUI(newTodo) {
     link.className = "delete-item";
     link.innerHTML = "<i class = 'fa fa-remove'></i>";
     listItem.className = "list-group-item d-flex justify-content-between";
-    //textimizi ekleme 
+
     listItem.appendChild(document.createTextNode(newTodo));
     listItem.appendChild(link);
     todoList.appendChild(listItem);
